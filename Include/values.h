859e8486e6a5421d98d73bdf21951d2f54a01c7b
PeriodGamingYT <54007341+PeriodGamingYT@users.noreply.github.com>
2019-08-11 16:22:29 -0600

    Update values.h

--- Include/values.h
+++ Include/values.h
@@ -1,5 +1,3 @@
-#ifndef __INCLUDE__VALUES.H
-#include __INCLUDE__VALUES.H
 typedef unsigned char uint_8t;
 typedef unsigned short uint_16t;
 typedef unsigned int uint_32t;
@@ -10,4 +8,3 @@ typedef short int_16t;
 typedef int int_32t;
 typedef long long int_64t;
 typedef bool int_1t
-#endif
