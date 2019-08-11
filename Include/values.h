209c3ae4e81f100b080ffb97f84891f9ba82d1a5
PeriodGamingYT <54007341+PeriodGamingYT@users.noreply.github.com>
2019-08-11 16:19:21 -0600

    Create values.h

---
+++ Include/values.h
@@ -0,0 +1,13 @@
+#ifndef __INCLUDE__VALUES.H
+#include __INCLUDE__VALUES.H
+typedef unsigned char uint_8t;
+typedef unsigned short uint_16t;
+typedef unsigned int uint_32t;
+typedef unsigned long long uint_64t;
+typedef unsigned bool uint_1t;
+typedef char int_8t;
+typedef short int_16t;
+typedef int int_32t;
+typedef long long int_64t;
+typedef bool int_1t
+#endif
