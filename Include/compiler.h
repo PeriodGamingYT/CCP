f35cee35793b0d0fbf4e558264a1e4f2e5baacef
PeriodGamingYT <54007341+PeriodGamingYT@users.noreply.github.com>
2019-08-12 15:44:59 -0600

    Create compiler.h
    Core functions of compiler.c (Will come later).

---
+++ Include/compiler.h
@@ -0,0 +1,11 @@
+#include "values.h"
+#include "numbers.h"
+#include <stdio.h>
+#include <string.h>
+int compileLine(char line[]) {
+int i, charLong, charStart;
+char *command, *value, *option;
+	while(line[i] != '\n' || line[i] != ';') {
+
+	} // Will come back later.
+}
