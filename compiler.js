ce6a92bf447ae46ddf9ed69f5dab3edc787adaa1
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2020-01-15 20:23:09 -0700

    Made working.
    See the first comment.

--- compiler.js
+++ compiler.js
@@ -1,8 +1,54 @@
+/* The regular expressions is being a RUDE BITCH */
+
 function lex(str) {
   if(typeof(str) != 'string') {
     console.error('You need to input a string');
     return -1;
   }
-  var lxe = str => str.split(/\b( |	|[*]|[/]|[+]|[-]|[(]|[)]|[[]|]|{|})\b/).map(s => s.trim()).filter(s => s.length);
-  return lxe(str);
+  var lxe = str => str.split(' ').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('\t').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('+').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('-').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('*').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('/').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('^').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('(').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split(')').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('[').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split(']').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('{').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('}').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('=').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  var lxe = str => str.split('`').map(s => s.trim()).filter(s => s.length);
+  var cLex = lxe(str);
+  str = cLex;
+  return cLex;
 }
