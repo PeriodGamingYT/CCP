d29f098588cdeb2ecd90586f2e0771f9de56507c
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2020-01-15 18:05:29 -0700

    lex() function made

--- compiler.js
+++ compiler.js
@@ -1,5 +1,8 @@
 function lex(str) {
+  if(typeof(str) != 'string') {
+    console.error('You need to input a string');
+    return -1;
+  }
   var lxe = str => str.split(/\b( |	|[*]|[/]|[+]|[-]|[(]|[)]|[[]|]|{|})\b/).map(s => s.trim()).filter(s => s.length);
-  return lxe;
+  return lxe(str);
 }
-
